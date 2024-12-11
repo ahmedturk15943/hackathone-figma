00f3c4710a88138b27a8188aa8aa89d9bf534dc9
ahmedturk15943 <ahmedturk748972@gmail.com>
2024-12-11 22:20:19 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomePage from './HomePage/page'
+const HomePagee = () => {
+  return (
+    <div>
+    <HomePage />
+    </div>
+  )
+}
+
+export default HomePagee
